3c0251fb1aa7fa6d95e7dc4c8ac1f07aaced6ff2
Luis Endres <luisendres@yahoo.com>
2021-07-07 17:10:29 -0700

    functions intermediate

--- fundamentals/fundamentals/functions_intermediate_i.py
+++ fundamentals/fundamentals/functions_intermediate_i.py
@@ -1,3 +1,4 @@
+# #1 ------------------------------------------------------------------------UPDATE VALUES IN DICTIONARIES AND LISTS------------------------------------------------------------------------
 x = [ [5,2,3], [10,8,9] ] 
 students = [
     {'first_name':  'Michael', 'last_name' : 'Jordan'},
@@ -24,3 +25,67 @@ print(sports_directory['soccer'][0])
 #4.Change the value 20 in z to 30
 z[0]["y"] = 30
 print(z[0]["y"])
+
+#2 ------------------------------------------------------------------------ITERATE THROUGH A LIST OF DICTIONARIES------------------------------------------------------------------------
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(some_list):
+    for i in range(0,len(some_list)):
+        for key, val in some_list[i].items():
+            print(key, "-",val)
+iterateDictionary(students)
+
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+#3 ------------------------------------------------------------------------GET VALUES FROM A LIST OF DICTIONARIES------------------------------------------------------------------------
+def iterateDictionary2(key_name, some_list):
+    for i in range(0,len(some_list)):
+        print(some_list[i][key_name])
+
+iterateDictionary2('first_name',students)
+iterateDictionary2('last_name',students)
+
+#4 ------------------------------------------------------------------------ITERATE THOUGH A DICTIONARY WITH LIST VALUES------------------------------------------------------------------------
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    counter = 0
+    for i in some_dict:
+        print(i)
+        for j in range(0,len(some_dict[i])):
+            print(some_dict[i][j])
+            
+printInfo(dojo)
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
